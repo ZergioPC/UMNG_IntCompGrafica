4d511cbbf1032ec1b0f801bc809cb9f8cae63249
Sergio Palacios <sergid26pal@gmail.com>
2024-01-26 15:15:11 -0500

    Codigo JS terminado

--- Actividad 01/Conversor Polar-Cartesiano/script.js
+++ Actividad 01/Conversor Polar-Cartesiano/script.js
@@ -7,21 +7,26 @@ const $btn_toPolar = document.querySelector("#toPolar");;
 const $btn_convert = document.querySelector("#btn_convert");
 const $btn_volver = document.querySelector("#btn_back");
 
-const $x = document.querySelector("#imputX");
-const $y = document.querySelector("#imputY");
+const $num1 = document.querySelector("#imputX");
+const $num2 = document.querySelector("#imputY");
 
 const $span_option = document.querySelector("#option");
 const $span_optionRt = document.querySelector("#option_rt");
 const $span_req = document.querySelector("#req");
 const $span_res = document.querySelector("#res");
 
-//Variables Internas
-let select;
+const $p_rta = document.querySelector(".rta");
+const $p_coord1 = document.querySelector("#coord1");
+const $p_coord2 = document.querySelector("#coord2");
 
+//Variables Internas
+let select = null;
+
+//Algoritmos de Conversión
 function convertPolar(x,y){
     let r = Math.sqrt((x*x)+(y*y));
     let a = (Math.atan(y/x))*(180/Math.PI);
-    return [r,a];
+    return [r.toFixed(2),a.toFixed(2)];
 };
 
 function convertCart(r,a){
@@ -30,3 +35,68 @@ function convertCart(r,a){
     return[x.toFixed(2),y.toFixed(2)];
 }
 
+//Selecvionar Conversión
+$btn_toCart.addEventListener("click",()=>{
+    select = "cartesianas";
+    $span_option.innerHTML = select;
+    $p_coord1.innerHTML = "<i>r</i>";
+    $p_coord2.innerHTML = "&#952;";
+    $section_sel.classList.add("off");
+    $section_conv.classList.remove("off");
+});
+
+$btn_toPolar.addEventListener("click",()=>{
+    select = "polares";
+    $span_option.innerHTML = select;
+    $p_coord1.innerHTML = "<i>x</i>";
+    $p_coord2.innerHTML = "<i>y</i>";
+    $section_sel.classList.add("off");
+    $section_conv.classList.remove("off");
+});
+
+//Volver al Menú
+$btn_volver.addEventListener("click",()=>{
+    select = null;
+    $section_conv.classList.add("off");
+    $section_sel.classList.remove("off");
+    $p_rta.classList.add("off");
+});
+
+//Conversión
+function htmlInsert(rta){
+    let aux;
+    let aux2;
+
+    if(select == "cartesianas"){
+        aux = `${$num2.value}°`;
+    }else{
+        aux = $num2.value;
+    }
+
+    if(select == "polares"){
+        aux2 = `${rta[1]}°`;
+    }else{
+        aux2 = rta[1];
+    }
+
+    $span_optionRt.innerHTML = select;
+    $span_req.innerHTML = `(${$num1.value},${aux})`;
+    $span_res.innerHTML = `(${rta[0]},${aux2})`;
+}
+
+$btn_convert.addEventListener("click",()=>{
+    let rta;
+
+    switch(select){
+        case "cartesianas":
+            rta = convertCart(parseFloat($num1.value),parseFloat($num2.value));
+            break;
+        case "polares":
+            rta = convertPolar(parseFloat($num1.value),parseFloat($num2.value));
+            break;
+    }
+    
+    htmlInsert(rta);
+    console.log(rta);
+    $p_rta.classList.remove("off");
+});
