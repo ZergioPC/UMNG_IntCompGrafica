1456b359f1fc5dd748b433a95a9b9258eb5fe70c
Sergio UMNG <sergid26pal@gmail.com>
2024-02-01 13:40:56 -0500

    SEMANA 2: Inicio de la actividad, Creación de HTML, JS

---
+++ Actividad 02/parte1.js
@@ -0,0 +1,83 @@
+const rta = {
+    books:[
+        {
+            titulo:"Computer graphics with OpenGL",
+            autor:"Hearn, Donald",
+            apa:"Computer graphics with OpenGL. Donald Hearn, M. Pauline Baker. Tercera Edicion, Editorial Prentice Hall, 2004.(biblioteca UMNG).",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=14863&query_desc=kw%2Cwrdl%3A%20Computer%20graphics%20with%20OpenGL"
+        },
+        {
+            titulo:"Computer graphics : C version",
+            autor:"Hearn, Donald",
+            apa:"Computer graphics : C version. Donald Hearn, M. Pauline Baker. Segunda Edicion, Editorial Prentice Hall, 1997. (biblioteca UMNG).",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=6351&query_desc=kw%2Cwrdl%3A%20Computer%20graphics%20%3A%20C%20version"
+        },
+        {
+            titulo:"Computer graphics : principles and practice",
+            autor:"Van Dam, Andries, Hughes, John F, Feiner, Steven K  Foley & James D",
+            apa:"Computer graphics: principles and practice, Foley, James D., el al.. Segunda Edicion, Editorial Addison Wesley, 1997. (biblioteca UMNG).",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=30131&query_desc=kw%2Cwrdl%3A%20Computer%20graphics%3A%20principles%20and%20practice"
+        },{
+            titulo:"Interactive computer graphics : a top-down approach with openGL",
+            autor:" Angel, Edward",
+            apa:"Interactive Computer Graphics, A top-down approach with OpenGL. Edward Angel. Editorial Addison-Wesley, 2009. (biblioteca UMNG).",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=5955&query_desc=kw%2Cwrdl%3A%20Interactive%20Computer%20Graphics%2C%20A%20top-down%20approach%20with%20OpenGL"
+        },{
+            titulo:"OpenGL : A primer",
+            autor:" Angel, Edward",
+            apa:"OpenGL: A Primer. Edward Angel. Editorial: Addison-Wesley, 2008. (biblioteca UMNG).",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=14958&query_desc=kw%2Cwrdl%3A%20OpenGL%3A%20A%20Primer"
+        },{
+            titulo:"Mathematics for 3D game programming and computer graphics",
+            autor:" Lengyel, Eric",
+            apa:"Mathematics for 3D game programming and computer graphics. Eric Lengyel. 3a ed. Boston, MACengage, 2012. (biblioteca UMNG)..",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=11882&query_desc=kw%2Cwrdl%3A%20Mathematics%20for%203D%20game%20programming%20and%20computer%20graphics"
+        }
+    ],
+    extras:[
+        {
+            titulo:"Mathematics for 3D game programming and computer graphics",
+            autor:" Lengyel, Eric",
+            apa:"Mathematics for 3D game programming and computer graphics. Eric Lengyel. 3a ed. Boston, MACengage, 2012. (biblioteca UMNG)..",
+            url:"https://catalogo.unimilitar.edu.co/cgi-bin/koha/opac-detail.pl?biblionumber=11882&query_desc=kw%2Cwrdl%3A%20Mathematics%20for%203D%20game%20programming%20and%20computer%20graphics"
+        }
+    ],
+    glosario:[
+        {
+            word:"Nodo",
+            p:"En informática y en telecomunicación, de forma muy general, un nodo es un punto de intersección, conexión o unión de varios elementos que confluyen en el mismo lugar."
+        },{
+            word:"Fresnel Reflection",
+            p:"Fenómeno en gráficos por computadora que modela cómo varía la reflectividad de una superficie según el ángulo de visión, contribuyendo a efectos realistas en renderizado."
+        },{
+            word:"Bump Mapping",
+            p:"Técnica que simula la rugosidad de una superficie mediante el uso de mapas de altura para crear sombras y resaltes, mejorando la apariencia de detalles a nivel de píxeles."
+        },{
+            word:"Caustics",
+            p:"Fenómeno de refracción de la luz que crea patrones de iluminación complejos en superficies, utilizado para simular efectos visuales como los patrones de luz en el fondo de una piscina."
+        },{
+            word:"Volumetric Rendering",
+            p:"Técnica que permite representar y visualizar objetos tridimensionales mediante el modelado y renderizado de volúmenes, comúnmente utilizado en visualización médica y simulaciones científicas."
+        },{
+            word:"Ray Tracing Acceleration",
+            p:"Algoritmos y estructuras de datos diseñadas para acelerar el proceso de trazado de rayos, mejorando la eficiencia en la generación de imágenes."
+        },{
+            word:"Anisotropic Filtering",
+            p:"Método de filtrado que mejora la calidad de las texturas al considerar la dirección de la superficie, reduciendo la distorsión y mejorando la apariencia en ángulos oblicuos."
+        },{
+            word:"Global Illumination",
+            p:"Técnica que simula la interacción de la luz en una escena, incluyendo efectos de iluminación indirecta como reflexiones y refracciones, para lograr una representación más realista."
+        },{
+            word:"Particle System:",
+            p:"Sistema utilizado para simular y renderizar partículas en movimiento, ampliamente empleado en efectos visuales como fuego, humo y fluidos."
+        },{
+            word:"Non-Photorealistic Rendering (NPR)",
+            p:"Enfoque que busca crear imágenes con estilos artísticos no realistas, como pinturas o dibujos, en lugar de replicar la apariencia fotográfica."
+        },{
+            word:"Inverse Kinematics",
+            p:" Técnica utilizada en animación que calcula las posiciones de las articulaciones de un modelo tridimensional para alcanzar una posición específica, facilitando la animación realista de personajes."
+        }
+    ]
+}
+
+export {rta};
---
+++ Actividad 02/script.js
@@ -0,0 +1,41 @@
+import {rta} from "./parte1.js";
+
+const $rta1 = document.querySelector("#rta_1");
+const $rta2 = document.querySelector("#rta_2");
+const $rta3 = document.querySelector("#rta_3");
+
+function insert(x,div){
+    let frag = document.createDocumentFragment();
+    let aux = frag.appendChild(document.createElement("ul"))
+
+    for(let i=0; i<x.length; i++){
+        let e = x[i];
+        aux.innerHTML +=
+        `<li>
+            <h4>${e.titulo}</h4>
+            <h5>by ${e.autor}</h5>
+            <p>APA:${e.apa}</p>
+            <a href="${e.url}">Enlace al Documento</a>
+        </li>`;
+    }
+    div.appendChild(frag);
+};
+
+function glosario(x,div){
+    let frag = document.createDocumentFragment();
+    let aux = frag.appendChild(document.createElement("ol"))
+
+    for(let i=0; i<x.length; i++){
+        let e = x[i];
+        aux.innerHTML +=
+        `<li>
+            <p class="dic_bold">${e.word}:</p>
+            <p clas="dic_rta">${e.p}</p>
+        </li>`;
+    }
+    div.appendChild(frag);
+}
+
+insert(rta.books,$rta1);
+insert(rta.extras,$rta2);
+glosario(rta.glosario,$rta3);
