f52dddd14f7adda878137456d5af2155eeaf4429
Sergio UMNG <sergid26pal@gmail.com>
2024-02-15 13:01:44 -0500

    CLASE 4: Graficar mediante un arreglo tres funciones, carpeta de la clase creada

---
+++ Semana_4/Actividad_1/menu.py
@@ -0,0 +1,42 @@
+import clases as clase;
+
+graficador = None;
+
+def retorno():
+    print("Desea ver otra grafica?");
+    print("1. Si \t 2.No");
+    sel = int(input(""));
+
+    if sel == 1:
+        menu();
+    else:
+        pass;
+
+def menu():
+    graficador  = clase.graficar();
+    
+    print("- - - Graficador de Funciones - - -");
+    print("Seleccione la función a graficar");
+    print("1. Función lineal f(x)= x");
+    print("2. Función Parabolica f(x)= x^2")
+    print("3. Función Seno f(x)= sin(x)")
+    
+    sel = int(input(""));
+
+    if sel == 1:
+        graficador.lineal();
+        graficador.draw();
+        retorno();
+    elif sel == 2:
+        graficador.parabola();
+        graficador.draw();
+        retorno();
+    elif sel == 3:
+        graficador.sin();
+        graficador.draw();
+        retorno();
+    else:
+        print("Valor no valido\n\n\n")
+        retorno();
+
+menu();
